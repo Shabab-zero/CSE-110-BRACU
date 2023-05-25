fa064ae580067cab3ba12283c0745b0aa83349ce
Shabab-zero <131799858+Shabab-zero@users.noreply.github.com>
2023-05-26 02:34:43 +0600

    Delete Assignment-1 directory

--- Assignment-1/Task-1.py
+++
@@ -1,8 +0,0 @@
-# Task-1
-
-number1 = int(input("Enter a number: "))
-number2 = int(input("Enter another number: "))
-
-print("Sum =", number1 + number2)
-print("Product =", number1 * number2)
-print("Difference =", number1 - number2)
--- Assignment-1/Task-6.py
+++
@@ -1,9 +0,0 @@
-# Task-6
-
-number = int(input("Enter a number: "))
-
-if number % 2 == 0 or number % 5 == 0:
-    print(number)
-
-else:
-    print("Not a multiple of 2 OR 5")
